4dc13eb4e80a191004ebdcba60ff4e5ae0ab98e7
Lucas Melo <lucas.s.melo@integer.pt>
2025-05-28 00:37:46 +0100

    Auth

--- JSCustomer/src/main/java/com/br/JSCustomer/config/FireBaseConfigManager.java
+++ JSCustomer/src/main/java/com/br/JSCustomer/config/FireBaseConfigManager.java
@@ -1,11 +1,14 @@
 package com.br.JSCustomer.config;
 
+import com.br.JSCustomer.rest.api.OrderServices;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,22 +18,32 @@ import java.io.InputStream;
 @Configuration
 public class FireBaseConfigManager {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderServices.class);
+
     @PostConstruct
     public void initialize() throws IOException {
 
+        logger.debug("Iniciou o metodo de firebase");
         InputStream serviceAccount = getClass()
                 .getClassLoader()
                 .getResourceAsStream("jfood-4c3be-firebase-adminsdk-ltl00-7d35246665.json");
+
+        logger.debug("Arquivo "+serviceAccount);
 
         if (serviceAccount == null) {
             throw new IOException("Arquivo de credenciais Firebase não encontrado no classpath.");
         }
 
+        logger.debug("Credencial "+GoogleCredentials.getApplicationDefault());
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.getApplicationDefault())
                 .build();
+        logger.debug("Credencial  2"+GoogleCredentials.getApplicationDefault());
+
 
         FirebaseApp.initializeApp(options);
+        logger.debug("Credencial INIT");
 
     }
 
--- JSCustomer/src/main/java/com/br/JSCustomer/rest/api/OrderServices.java
+++ JSCustomer/src/main/java/com/br/JSCustomer/rest/api/OrderServices.java
@@ -6,6 +6,8 @@ import com.br.JSCustomer.model.entity.Order;
 import com.br.JSCustomer.model.entity.Product;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +22,8 @@ public class OrderServices {
 
     @Autowired
     private IOrderManager orderManager;
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderServices.class);
 
     @PostMapping("/createOrder")
     public ResponseEntity<Order> createOrder(@RequestBody Order order) {
